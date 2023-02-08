7c2e5065151857c53a5da67a52185d7470ca1ee2
Oseni <Kayode.Oseni@mountsinai.org>
2023-02-08 15:57:44 -0500

    realized that full utilization with for loop can be made towards listItems in theater.js; made changes and repositioned a few tags

--- src/components/Theater.js
+++ src/components/Theater.js
@@ -9,27 +9,39 @@ const Theater = ({ listItems, theater }) => {
 
     // external for-loop that encompasses return statement
     for (let i = 0; i < listItems.length; i++) {
-
-        return (
-            <div className="theater">
-                <div className="screen"> </div>
-
-                {
-                   listItems[i].seats.map((row, rowIndex) => {
+        if (listItems[i].movie === theater.movie) {
+            log(listItems[i], listItems[i].seats);
+            listItems[i].seats.map((row, rowIndex) => {
+                // conditional
+                // log(row, rowIndex);
+                // log(Array.isArray(row))
+                row.map((seat, seatIndex) => {
                     // log(row, rowIndex);
-                    // log(Array.isArray(row))
-                    row.map((seat, seatIndex) => {
-                        log(row, rowIndex);
-                        log(seat, seatIndex);
-                    })
-                   }) 
-                }
-                <Row>
-                    <Seat> </Seat>
-                </Row>
-            </div>
-        );
+                    // log(seat, seatIndex);
+                    return (
+                        <div className="theater">
+                            <div className="screen"> </div>
+                            <Row className="row" key={rowIndex} value={row}>
+                                <Seat
+                                    className="seat"
+                                    key={seatIndex}
+                                    value={seat}
+                                ></Seat>
+                            </Row>
+                            ;
+                        </div>
+                    );
+                });
+            });
+        }
     }
 };
 
 export default Theater;
+
+/*
+NOTES
+- issue with rendering as soon as page loads.
+- issue logging 'The Lion King' as the first movie before selecting another movie.
+
+*/
